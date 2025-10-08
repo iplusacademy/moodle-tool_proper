a38b666b6e6b434aeaa807d3625b6d5a3b73865e
Renaat Debleu <rdebleu@eWallah.net>
2025-10-08 07:02:39 +0000

    Version update

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_proper';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v5.0.2';
-$plugin->version = 2025091400;
+$plugin->release = 'v5.1.1';
+$plugin->version = 2025100800;
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 501];
